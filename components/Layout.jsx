e4accd56e2f3c92eacf79c40539eabbacd02b63d
Abdel <abdelmawla.souat@gmail.Com>
2021-07-29 11:48:36 +0200

    Add the layout component

---
+++ components/Layout.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+import Header from './Header';
+import css from './Layout.module.css';
+
+// eslint-disable-next-line react/prop-types
+const Layout = ({ children }) => (
+  <>
+    <Head>
+      <title>Xelp</title>
+    </Head>
+
+    <Header />
+
+    <main>
+      <div className={css.container}>{children}</div>
+    </main>
+  </>
+);
+
+export default Layout;
