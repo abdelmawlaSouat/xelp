36f166613fb2dad5478a81578f7b48724599873d
Abdel <abdelmawla.souat@gmail.Com>
2021-07-29 11:45:46 +0200

    override the basic App component

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import '../styles/global.css';
+
+// eslint-disable-next-line react/prop-types
+function Xelp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default Xelp;
